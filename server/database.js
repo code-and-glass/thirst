aea906f6e077aa4346effd5808bfa40690adc198
Artem Bakalov <artem.bakalov@gmail.com>
2015-10-17 19:54:05 -0500

    close 13

---
+++ server/database.js
@@ -0,0 +1,131 @@
+var recommend = require('./recommender.js');
+var db;
+
+//Checks if deployed or local
+if(process.env.GRAPHENEDB_URL){
+
+  var url = require('url').parse(process.env.GRAPHENEDB_URL);
+
+  db = require("seraph")({
+    server: url.protocol + '//' + url.host,
+    user: url.auth.split(':')[0],
+    pass: url.auth.split(':')[1]
+  });
+} else {
+  var config = require('../config.js');
+  db = require("seraph")({
+    server: "http://localhost:7474",
+    user: config.neo4jAuth.user,
+    pass: config.neo4jAuth.password //your password here
+  });  
+}
+
+
+
+
+  // var array = [];
+  // var matrix = [];
+  // var batchingDone = false;
+  
+  
+
+  
+//    var read = function(err, relationship) {
+//    db.rel.read(relationship.id, function(err, readRelationship) {
+//       var rating = readRelationship.properties.rating;
+//       console.log(rating);
+//       //array.push(rating);
+//       //console.log(array);
+//     });
+//   };
+
+//   var write = function(err, relationship) {
+//  db.rel.read(relationship.id, function(err, readRelationship) {
+//     var rating = readRelationship.properties.rating;
+//     console.log(rating);
+//     array.push(rating);
+//     console.log(array);
+//     resultMatrix = listToMatrix(array, 4);
+//     console.log(resultMatrix);
+
+//   });
+// };
+
+var listToMatrix = function(list, elementsPerSubArray) {
+  var matrix = [], i, k;
+  for (i = 0, k = -1; i < list.length; i++) {
+    if (i % elementsPerSubArray === 0) {
+      k++;
+      matrix[k] = [];
+    }
+    matrix[k].push(list[i]);
+  }
+  return matrix;
+};
+
+
+
+  db.batch(function(txn) {
+    console.log('batch start');
+  
+  var user1 = txn.save({name: 'Artem'});
+  var user2 = txn.save({name: 'Ben'});
+  var user3 = txn.save({name: 'Victoria'});
+  var user4 = txn.save({name: 'Igor'});
+
+  var likedDrinks = txn.save([
+    { drinkName: 'maple syrup'},
+    { drinkName: 'blood of my enemies'},
+    { drinkName: 'Dihydrogen Monoxide'},
+    { drinkName: 'liquid cocaine'}
+  ]);
+
+  txn.relate(user1, 'likes', likedDrinks[1], {rating: '5'});
+  txn.relate(user1, 'likes', likedDrinks[2], {rating: '1'});
+  txn.relate(user1, 'likes', likedDrinks[0], {rating: '3'});
+  txn.relate(user1, 'likes', likedDrinks[3], {rating: '3'});
+
+  txn.relate(user2, 'likes', likedDrinks[1], {rating: '2'});
+  txn.relate(user2, 'likes', likedDrinks[2], {rating: '5'});
+  txn.relate(user2, 'likes', likedDrinks[0], {rating: '3'});
+  txn.relate(user2, 'likes', likedDrinks[3], {rating: '4'});
+
+  txn.relate(user3, 'likes', likedDrinks[1], {rating: '5'});
+  txn.relate(user3, 'likes', likedDrinks[2], {rating: '4'});
+  txn.relate(user3, 'likes', likedDrinks[0], {rating: '1'});
+  txn.relate(user3, 'likes', likedDrinks[3], {rating: '2'});
+
+  txn.relate(user4, 'likes', likedDrinks[1], {rating: '3'});
+  txn.relate(user4, 'likes', likedDrinks[2], {rating: '3'});
+  txn.relate(user4, 'likes', likedDrinks[0], {rating: '3'});
+  txn.relate(user4, 'likes', likedDrinks[3], {rating: '1'});
+
+  batchingDone = ('batching done');
+  
+  
+  },
+  
+  function(err, results) {
+    
+
+    var array = [];
+    results.forEach(function(node) {
+      if (node.type && node.type === 'likes') {
+        array.push(node.properties.rating);
+      }
+    });
+    var resultsMatrix = listToMatrix(array, 4);
+    results = resultsMatrix;
+    console.log('batching committed');
+    console.log(results);
+    var rowLabels = ['Artem', 'Ben', 'Victoria', 'Igor'];
+    var colLabels = ['blood of my enemies', 'Dihydrogen Monoxide', 'maple syrup', 'liquid cocaine'];
+    var model = recommend.model(results, rowLabels, colLabels);
+    console.log(model.rankAllItems('Victoria'));
+    return results;
+  
+});
+
+
+
+
---
+++ server/recommender.js
@@ -0,0 +1,41 @@
+
+var Recommender = require('likely');
+
+// var inputMatrix = [ [ 5, 5, 5, 5 ], //
+//                     [ 5, 5, 5, 5 ],
+//                     [ 5, 5, 5, 5 ]
+//                   ];
+
+// var rowLabels = ['John', 'Sue', 'Joe', 'suss']; //users
+// var colLabels = ['vodka', 'water', 'absinthe', 'maple syrup']; //items
+
+module.exports.model = function(matrix, rowLabels, colLabels) {
+   return Recommender.buildModel(matrix, rowLabels, colLabels);
+};
+
+// //Retrieve a list of all items not already rated by a user, sorted by estimated ratings using labels.
+module.exports.recommendations = function(user) {
+  return model.recommendations(user); 
+};
+
+// //Retrieve a list of all items not already rated by a user, sorted by estimated ratings without labels.
+module.exports.recommendations = function() {
+  return model.recommendations(0);
+};
+
+// //Retrieve a list of all items, sorted by the ratings for a given user (both estimated and actual), using labels.
+module.exports.allItems = function(user) {
+  return model.rankAllItems(user);
+};
+
+// //Retrieve a list of all items, sorted by the ratings for a given user (both estimated and actual) without labels.
+// //var allItems = model.rankAllItems(0);
+
+// //BIAS HANDLING
+
+// //var bias = Recommender.calculateBias(input);
+
+// // Build the model using the training set and considering the bias
+// //var model = Recommender.buildModelWithBias(trainingSet, bias);
+
+// console.log(allItems);
