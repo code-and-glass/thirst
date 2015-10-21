bf2f480316a1d0948626ad829fe797639406fb81
Igor <istefanco@gmail.com>
2015-10-20 22:03:24 -0500

    changes for heroku

--- app.js
+++ app.js
@@ -28,6 +28,13 @@ app.use('/users', users);
 
 module.exports = app;
 
+<<<<<<< HEAD
+=======
+app.listen(process.env.PORT || '3000');
+
+// The following needs to be reviewed and removed:
+
+>>>>>>> changes for heroku
 
 
 //********************* AUTH testing to be moved later ***************************
@@ -101,3 +108,44 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/login');
 }
 //**************************************************
+<<<<<<< HEAD
+=======
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+
+module.exports = app;
+
+>>>>>>> changes for heroku
