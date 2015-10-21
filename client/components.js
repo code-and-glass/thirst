78b698190cf14e264c4aa40441238e82a8e3a929
Ben Hejkal <ben.hejkal@gmail.com>
2015-10-20 22:24:25 -0500

    More work on routing and front end of auth.

--- client/components.js
+++ client/components.js
@@ -111,26 +111,10 @@ const Login = React.createClass({
   _handleTouchTap() {
   },
 });
-
-/*
-CODE TO BE USED WHEN SIGNING PEOPLE IN
 // https://developers.google.com/identity/sign-in/web/sign-in
 // https://developers.google.com/identity/sign-in/web/backend-auth
-<script>
- function onSignIn(googleUser) {
-   // Useful data for your client-side scripts:
-   var profile = googleUser.getBasicProfile();
-   console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-   console.log("Name: " + profile.getName());
-   console.log("Image URL: " + profile.getImageUrl());
-   console.log("Email: " + profile.getEmail());
 
-   // The ID token you need to pass to your backend:
-   var id_token = googleUser.getAuthResponse().id_token;
-   console.log("ID Token: " + id_token);
- };
-</script>
-*/
+
 
 
 
