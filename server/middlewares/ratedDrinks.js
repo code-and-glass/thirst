ece5c560663838607cb42726480f4c4d5731dd59
Artem Bakalov <artem.bakalov@gmail.com>
2015-11-05 23:25:22 -0600

    Fix rated drinks route.

--- server/middlewares/ratedDrinks.js
+++ server/middlewares/ratedDrinks.js
@@ -11,13 +11,15 @@ app.get('/drinks/ratedDrinks', function(req, res, next) {
    User.getUser({googleId: id}, function(err, user) {
      if (err) throw err;
      var cypher = "MATCH (b:User {googleId: 'KEY'})-[r:RATED]->(m:Drink)\n"+
-                  "WHERE  ((b)-[:RATED]->(m))\n"+
+                  //"WHERE  ((b)-[:RATED]->(m))\n"+
                   "RETURN m,r";
      cypher = cypher.replace('KEY', id);
+     console.log(cypher);
      User.query(cypher, null, function(results) {
 
        
        responseData = {};
+       console.log(results);
        responseData.results = results.map(function(item) {
         return { name: item.m.name, id: item.m.id, rating: item.r.properties.rating};
       });
